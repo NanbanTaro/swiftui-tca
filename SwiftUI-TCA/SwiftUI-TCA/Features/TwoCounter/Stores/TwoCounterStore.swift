ee0017a531295fb63550a6717d99b31674910edc
NanbanTaro <>
2023-05-03 15:21:55 +0900

    fix TwoCounters

--- SwiftUI-TCA/SwiftUI-TCA/Features/TwoCounter/Stores/TwoCounterStore.swift
+++ SwiftUI-TCA/SwiftUI-TCA/Features/TwoCounter/Stores/TwoCounterStore.swift
@@ -1,9 +1,30 @@
 //
-//  TwoCounter.swift
+//  TwoCounterStore.swift
 //  SwiftUI-TCA
 //
 //  Created by NanbanTaro on 2023/05/03.
 //  
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct TwoCounters: ReducerProtocol {
+    struct State: Equatable {
+        var counter1 = Counter.State()
+        var counter2 = Counter.State()
+    }
+
+    enum Action: Equatable {
+        case counter1(Counter.Action)
+        case counter2(Counter.Action)
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Scope(state: \.counter1, action: /Action.counter1) {
+            Counter()
+        }
+        Scope(state: \.counter2, action: /Action.counter2) {
+            Counter()
+        }
+    }
+}
--- SwiftUI-TCA/SwiftUI-TCA/Features/TwoCounter/Views/TwoCounterView.swift
+++ SwiftUI-TCA/SwiftUI-TCA/Features/TwoCounter/Views/TwoCounterView.swift
@@ -6,4 +6,27 @@
 //  
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct TwoCountersView: View {
+    let store: StoreOf<TwoCounters>
+
+    var body: some View {
+        HStack {
+            Text("Counter 1")
+            Spacer()
+            CounterView(
+                store: self.store.scope(state: \.counter1, action: TwoCounters.Action.counter1)
+            )
+        }
+
+        HStack {
+            Text("Counter 2")
+            Spacer()
+            CounterView(
+                store: self.store.scope(state: \.counter2, action: TwoCounters.Action.counter2)
+            )
+        }
+    }
+}
